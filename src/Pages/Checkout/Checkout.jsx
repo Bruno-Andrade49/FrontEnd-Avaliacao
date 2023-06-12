abb14b6347c440cb00ade4f5501e873f7822dedf
Bruno Andrade <b.emanueandrade@hotmail.com>
2023-06-12 10:11:21 -0300

    Subindo projeto para o git da impulse

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -12,7 +12,7 @@ function Checkout() {
             <form>
                 <div class="container-form">
                     <div style={{ display: "block", width: "500px", paddingTop: "50px" }}>
-                        <label style={{fontSize: "22px"}}>Insira o códig do produto</label>
+                        <label style={{ fontSize: "22px" }}>Insira o códig do produto</label>
                         <div class="input-group mb-3">
 
                             <input type="text" class="form-control" placeholder="Cód prod" aria-label="Recipient's username" aria-describedby="basic-addon2" />
